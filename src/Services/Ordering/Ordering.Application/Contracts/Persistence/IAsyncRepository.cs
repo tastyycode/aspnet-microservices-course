eb4ff0afaf911d94faa10cffa85e8845535239b0
Carlos C <contact@carloscolin.me>
2022-06-30 00:29:16 -0500

    Made some null arguments

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -13,15 +13,15 @@ namespace Ordering.Application.Contracts.Persistence
         Task<IReadOnlyList<T>> GetAllAsync();
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
         Task<IReadOnlyList<T>> GetAsync(
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            string includeString = null,
+            Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string? includeString = null,
             bool disableTracking = true
         );
         Task<IReadOnlyList<T>> GetAsync(
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            List<Expression<Func<T, object>>> includes = null,
+            Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            List<Expression<Func<T, object>>>? includes = null,
             bool disableTracking = true
         );
         Task<T> GetByIdAsync(int id);
