1525e77a5f5dbbfac43eb49210c46a83dfefed15
Carlos C <contact@carloscolin.me>
2022-07-02 13:56:21 -0500

    Added comment about pipeline behaviours order

--- src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -14,6 +14,8 @@ namespace Ordering.Application
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
+            // The order in which you declare the Pipelines behaviours,
+            // is the order in which they will be executed.
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
