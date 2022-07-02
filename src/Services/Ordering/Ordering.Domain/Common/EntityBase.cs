043e7c9fe29f1532759754ff0dfb7271081041dc
Carlos C <contact@carloscolin.me>
2022-07-02 13:55:59 -0500

    Make some properties nullable

--- src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -9,9 +9,9 @@ namespace Ordering.Domain.Common
     public abstract class EntityBase
     {
         public int Id { get; protected set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-        public string LastModifiedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
         public DateTime? LastModifiedDate { get; set; }
     }
 }
--- src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -13,19 +13,19 @@ namespace Ordering.Domain.Entities
         public decimal TotalPrice { get; set; }
 
         // Billing address
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailAddress { get; set; }
-        public string AddressLine { get; set; }
-        public string Country { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? AddressLine { get; set; }
+        public string? Country { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
 
         // Payment
-        public string CardName { get; set; }
-        public string CardNumber { get; set; }
-        public string Expiration { get; set; }
-        public string Cvv { get; set; }
-        public int PaymentMethod { get; set; }
+        public string? CardName { get; set; }
+        public string? CardNumber { get; set; }
+        public string? Expiration { get; set; }
+        public string? Cvv { get; set; }
+        public int? PaymentMethod { get; set; }
     }
 }
