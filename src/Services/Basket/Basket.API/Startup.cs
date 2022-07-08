5cff649b41cd1afae4e4a725c0d828b17bafc1c3
Carlos C <contact@carloscolin.me>
2022-07-08 01:09:38 -0500

    Support event messaging

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -13,6 +13,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
+using Basket.API.Mappings;
 
 namespace Basket.API
 {
@@ -28,25 +30,51 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region General Configuration
             services.AddScoped<IBasketRepository, BasketRepository>();
+            #endregion
 
+            #region gRPC configuration
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
-
             services.AddScoped<DiscountGrpcService>();
+            #endregion
 
+            #region Redis Configuration
             services.AddStackExchangeRedisCache(options =>
             {
                 string password = Configuration.GetValue<string>("CacheSettings:Password");
                 string connectionString = Configuration.GetValue<string>("CacheSettings:ConnectionString");
                 options.Configuration = $"{connectionString},password={password}";
             });
+            #endregion
+
+            #region AutoMapper Configuration
+            services.AddAutoMapper(options =>
+            {
+                options.AddProfile<BasketProfile>();
+            });
+            #endregion
+
+            #region MassTransit RabbitMQ Configuration
+            services.AddMassTransit((configure) =>
+            {
+                configure.UsingRabbitMq((context, cfg) =>
+                {
+                    string rabbitMqHost = Configuration.GetValue<string>("MessageBusSettings:RabbitMqHost");
+
+                    cfg.Host(rabbitMqHost);
+                });
+            });
+            #endregion
 
             services.AddControllers();
+            #region OpenAPI Configuration
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
             });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
